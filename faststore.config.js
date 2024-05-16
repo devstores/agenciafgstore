c3a3d8036c6c6d27cff85ec057ae03456c2e4944
emairinkfg <erick.mairinque@agenciafg.com.br>
2024-05-16 17:51:17 -0300

    config

--- faststore.config.js
+++ faststore.config.js
@@ -1,99 +1,80 @@
-
 module.exports = {
   seo: {
-  "title": "FastStore",
-  "description": "A fast and performant store framework",
-  "titleTemplate": "%s | FastStore",
-  "author": "FastStore"
-},
-
-  // Theming
-  theme: 'custom-theme',
-
-  // Ecommerce Platform
-  platform: 'vtex',
-
-  // Platform specific configs for API
+    title: "NextJSStore",
+    description: "Fast Demo Store",
+    titleTemplate: "%s | FastStore",
+    author: "Store Framework",
+  },
+  theme: "custom-theme",
+  platform: "vtex",
   api: {
     storeId: "storeframework",
-    workspace: 'master',
-    environment: 'vtexcommercestable',
-    hideUnavailableItems: true,
+    workspace: "master",
+    environment: "vtexcommercestable",
+    hideUnavailableItems: false,
     incrementAddress: false,
   },
-
-  // Default session
   session: {
     currency: {
-      code: "BRL",
-      symbol: "R$",
+      code: "USD",
+      symbol: "$",
     },
-    locale: "pt-BR",
-    channel: '{"salesChannel":1,"regionId":""}',
-    country: "BRA",
+    locale: "en-US",
+    channel: '{"salesChannel":"1","regionId":""}',
+    country: "USA",
     deliveryMode: null,
     addressType: null,
     postalCode: null,
     geoCoordinates: null,
     person: null,
   },
-
   cart: {
-    id: '',
+    id: "",
     items: [],
     messages: [],
     shouldSplitItem: true,
   },
-
-  // Production URLs
-  storeUrl: "https://storeframework.vtex.app",
-  secureSubdomain: "https://secure.vtexfaststore.com/",
+  storeUrl: "https://vtexfaststore.com",
+  secureSubdomain: "https://secure.vtexfaststore.com",
   checkoutUrl: "https://secure.vtexfaststore.com/checkout",
   loginUrl: "https://secure.vtexfaststore.com/api/io/login",
   accountUrl: "https://secure.vtexfaststore.com/api/io/account",
-
   previewRedirects: {
-    home: '/',
-    plp: "/testes%20e%20integra%C3%A7%C3%A3o%20(departamento)",
-    search: "/s?q=Prevent",
-    pdp: "/dp2101-c18-unica-unico/p",
+    home: "/",
+    plp: "/office",
+    search: "/s?q=headphone",
+    pdp: "/apple-magic-mouse/p",
   },
-
-  // Lighthouse CI
   lighthouse: {
-    server: process.env.BASE_SITE_URL || 'http://localhost:3000',
+    server: "http://localhost:3000",
     pages: {
-      home: '/',
-      pdp: "/dp2101-c18-unica-unico/p",
-      collection: "/testes%20e%20integra%C3%A7%C3%A3o%20(departamento)",
+      home: "/",
+      pdp: "/apple-magic-mouse/p",
+      collection: "/office",
     },
   },
-
-  // E2E CI
   cypress: {
     pages: {
-      home: '/',
-      pdp: "/dp2101-c18-unica-unico/p",
-      collection: "/testes%20e%20integra%C3%A7%C3%A3o%20(departamento)",
-      collection_filtered: "/testes%20e%20integra%C3%A7%C3%A3o%20(departamento)/?category-1=testes%20e%20integra%C3%A7%C3%A3o%20(departamento)&brand=Prevent&facets=category-1%2Cbrand%27",
-      search: "/s?q=Prevent",
+      home: "/",
+      pdp: "/apple-magic-mouse/p",
+      collection: "/office",
+      collection_2: "/technology",
+      collection_filtered:
+        "/office/?category-1=office&marca=acer&facets=category-1%2Cmarca",
+      search: "/s?q=orange",
     },
-    browser: 'electron',
   },
-
   analytics: {
-    // https://developers.google.com/tag-platform/tag-manager/web#standard_web_page_installation,
-    gtmContainerId: "GTM-1234567",
+    gtmContainerId: "GTM-PGHZ95N",
   },
-
   experimental: {
     nodeVersion: 18,
     cypressVersion: 12,
   },
-
+  account: "storeframework",
   vtexHeadlessCms: {
     webhookUrls: [
-      "https://agenciafg.myvtex.com/cms-releases/webhook-releases",
+      "https://storeframework.myvtex.com/cms-releases/webhook-releases",
     ],
   },
-}
+};
